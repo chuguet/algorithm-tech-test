bb536232ff9a93e6925059ffe06b99b4ff0fabb1
Carlos Huguet <carlos.huguet@denizen.io>
2019-10-02 16:35:38 +0200

    First and second ex done

--- src/main/java/ex2/Main.java
+++ src/main/java/ex2/Main.java
@@ -1,21 +1,38 @@
 package ex2;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(1, 2, 3);
+        List<Integer> nums = Arrays.asList(1,1,2,3,4,2,4,2,1,3,7);
         Integer k = 5;
-        Map<Tuple, Integer> kValues = kMethod(nums, k);
+        Set<Integer> hashSet = kMethodRecursive(nums, k, new HashSet<Integer>());
     }
 
-    private static Map<Tuple, Integer> kMethod (List<Integer> nums, Integer k) {
-        Map<Tuple, Integer> result = new HashMap<Tuple, Integer>();
-        return result;
+    private static Set<Integer> kMethod (List<Integer> nums, Integer k) {
+        Set<Integer> hashSet = new HashSet<Integer>();
+        for (Integer num : nums) {
+            if (hashSet.contains(k-num)) {
+                System.out.println("Pair with given sum " + k + " is (" + num + ", " + (k - num) + ")");
+            }
+            hashSet.add(num);
+        }
+
+        return hashSet;
+    }
+
+    private static Set<Integer> kMethodRecursive (List<Integer> nums, Integer k, Set<Integer> hashSet) {
+        if(!nums.isEmpty()) {
+            Integer num = nums.get(0);
+            if (hashSet.contains(k-num)) {
+                System.out.println("Pair with given sum " + k + " is (" + num + ", " + (k - num) + ")");
+            }
+            hashSet.add(num);
+            return kMethodRecursive(nums.subList(1, nums.size()), k, hashSet);
+        } else  {
+            return hashSet;
+        }
     }
 
 }
